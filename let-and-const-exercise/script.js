f5c46999b132f4d6e59772f9b3ddae87204f6140
root <root@DESKTOP-5DPJEDN.localdomain>
2022-12-18 10:04:43 -0500

    adding const and let exercise

---
+++ let-and-const-exercise/script.js
@@ -0,0 +1,23 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+const pi = 3.14;
+
+
+// * What is the difference between var and let?
+// you can redeclare car but not let.
+// var has function sscope and let has block scope.
+
+// * What is the difference between var and const?
+// you can reassaign var but no const.
+// you can redeclare car but not var.
+// var has function sscope and const has block scope.
+
+// * What is the difference between let and const?
+// you can reasaign let but not const.
+
+// * What is hoisting?
+// means declarations are processed before any code is executed. 
+// This means that declarations made at the top of a script or function
+// are treated as if they were made at the beginning of the script or function
+// regardless of where they actually appear in the code.
