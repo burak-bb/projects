eb6210c85b964187a804620b42dabc11578c38b8
burak <hosseiniburak@gmail.com>
2024-07-11 23:00:21 -0400

    biztime part 2

---
+++ express-biztime-2/db.js
@@ -0,0 +1,19 @@
+/** Database setup for BizTime. */
+require('dotenv').config();
+const express = require('express');
+
+
+const { Client } = require("pg");
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+client.connect();
+
+
+module.exports = client;
